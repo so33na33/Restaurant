807d99c63ae83a73966a1445a28daef46345aa6f
so33na33 <santhoshsona150@gmail.com>
2022-07-07 22:15:18 +0530

    searched successfully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { SearchRestaurantComponent } from './search-restaurant/search-restaurant.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    SearchRestaurantComponent
   ],
   imports: [
     BrowserModule,
